7c1cd22d4fac261db0b84e2472429dafa016de64
HimanshuChopra <himanshuchopra2008@outlook.com>
2024-04-06 23:24:58 +0530

    Initial Commit

---
+++ project_revival/src/test/java/com/example/project_revival/ProjectRevivalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.project_revival;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectRevivalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
